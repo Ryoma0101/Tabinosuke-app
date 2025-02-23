78206525684498e750247bc1a801b837e02964ca
masato626 <m.takiguchi.22.7.561@4nonome.com>
2025-02-23 17:37:50 +0900

    font-san

--- frontend/src/components/Atoms/Input/input.tsx
+++ frontend/src/components/Atoms/Input/input.tsx
@@ -11,7 +11,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex h-[42px] w-[325px] rounded-sm border input border-[var(--text-border-default,#3F3F46)] bg-background px-4 py-[11px] text-[16px] text-[var(--text-description,#A1A1AA)]",
+          "flex h-[42px] w-[325px] rounded-sm border input border-[#3F3F46] bg-background px-4 py-[11px] text-[16px] text-[#A1A1AA],font-sans]",
           className
         )}
         ref={ref}
