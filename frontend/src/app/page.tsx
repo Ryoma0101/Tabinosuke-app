d3dd147badfbe894895456489f3eda0dd7429cc0
masato626 <m.takiguchi.22.7.561@4nonome.com>
2025-02-23 21:36:17 +0900

    完成

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "@/components/Atoms/Header";
+import Day from "@/components/Atoms/Day";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-50 flex justify-center">
       <div className="w-full max-w-[768px] my-0 mx-[30px] bg-white">
         <Header text="計画" />
+        <Day day={1} date={new Date(2025, 1, 22)} />
         <main></main>
       </div>
     </div>
