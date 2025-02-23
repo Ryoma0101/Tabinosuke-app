2a1ecfbb61f88b341324aabd92a38096c02e2bd4
taku072002T <taku07200@gmail.com>
2025-02-23 16:54:37 +0900

    Added TwoPlaceDistance View

--- myapi/serializers.py
+++ myapi/serializers.py
@@ -3,3 +3,11 @@ from rest_framework import serializers
 
 class PlaceNameSerializer(serializers.Serializer):
     place_name = serializers.CharField()
+    place_bias = serializers.CharField()
+
+
+class TwoPlaceDistanceSerializer(serializers.Serializer):
+    place_lat_from = serializers.FloatField()
+    place_lng_from = serializers.FloatField()
+    place_lat_into = serializers.FloatField()
+    place_lng_into = serializers.FloatField()
--- myapi/urls.py
+++ myapi/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import PlaceNameView
+from .views import TwoPlaceDistanceView
 
 urlpatterns = [
     path('search-placename/', PlaceNameView.as_view(), name='placename'),
+    path('two-place-distance/', TwoPlaceDistanceView.as_view(), name='distance')
 ]
