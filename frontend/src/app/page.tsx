e8c1432943817013f811153f1f753826a37d80a1
masato626 <m.takiguchi.22.7.561@4nonome.com>
2025-02-23 22:45:52 +0900

    完成

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,20 +1,14 @@
 import React from "react";
 import Header from "@/components/Atoms/Header";
-import { Button } from "@/components/ui/button";
+import Day from "@/components/Atoms/Day";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-50 flex justify-center">
       <div className="w-full max-w-[768px] my-0 mx-[30px] bg-white">
         <Header text="計画" />
-        <main>
-          <div className="p-4 flex flex-row  gap-10">
-            <Button variant="outline" className="w-[136px]">
-              修正する
-            </Button>
-            <Button className="w-[136px] bg-[#3f3f46]">作成する</Button>
-          </div>
-        </main>
+        <Day day={1} date={new Date(2025, 1, 22)} />
+        <main></main>
       </div>
     </div>
   );
