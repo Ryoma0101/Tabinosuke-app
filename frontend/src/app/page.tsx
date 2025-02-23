8a4b8fab03d7755b1c2a2385f7c53a170fce192b
masato626 <m.takiguchi.22.7.561@4nonome.com>
2025-02-23 23:33:17 +0900

    完成

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="min-h-screen bg-gray-50 flex justify-center">
       <div className="w-full max-w-[768px] my-0 mx-[30px] bg-white">
         <Header text="計画" />
-        <Day day={1} date={new Date(2025, 1, 22)} />
+        <Day day={1} date={new Date(2025, 1, 25)} />
         <main></main>
       </div>
     </div>
