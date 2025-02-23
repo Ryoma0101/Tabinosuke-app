ede4ba24cf5e0a57864b2355b4b8140051faeb7a
Ryo <jukenohtani@gmail.com>
2025-02-23 16:15:46 +0900

    button完成

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import Header from "@/components/Atoms/Header";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-50 flex justify-center">
       <div className="w-full max-w-[768px] my-0 mx-[30px] bg-white">
         <Header text="計画" />
-        <main></main>
+        <main>
+          <div className="p-4 flex flex-row  gap-10">
+            <Button variant="outline" className="w-[136px]">
+              修正する
+            </Button>
+            <Button className="w-[136px] bg-[#3f3f46]">作成する</Button>
+          </div>
+        </main>
       </div>
     </div>
   );
