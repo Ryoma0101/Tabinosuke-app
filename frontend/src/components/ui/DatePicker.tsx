7a94a558437326010bdc588356d2f79ae8fe433f
masato626 <m.takiguchi.22.7.561@4nonome.com>
2025-02-24 17:02:48 +0900

    ピクセルパーフェクト

--- frontend/src/components/ui/DatePicker.tsx
+++ frontend/src/components/ui/DatePicker.tsx
@@ -20,7 +20,7 @@ export function DatePickerDemo() {
         <Button
           variant={"outline"}
           className={cn(
-            "w-[280px] justify-start text-left font-normal",
+            "w-[144px] justify-start text-left font-normal",
             !date && "text-muted-foreground"
           )}
         >
