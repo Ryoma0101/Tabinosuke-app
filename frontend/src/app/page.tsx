03c797dc70b47eb634c562129a9511b46bfea5c5
masato626 <m.takiguchi.22.7.561@4nonome.com>
2025-02-24 21:37:06 +0900

    終着地

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -6,6 +6,7 @@ import TransportSelector from "@/components/Molecules/TransportSelector";
 import Day from "@/components/Atoms/Day";
 import Schedule from "@/components/Atoms/Schedule";
 import DepartureCard from "@/components/Molecules/DepartureCard";
+import DestinationCard from "@/components/Molecules/DestinationCard";
 const jsonData = {
   time: "AM 5:00",
   priority: "高", // "低" または "中" も指定可能
@@ -29,6 +30,11 @@ export default function Home() {
           </div>
           <div className="p-6 gap-6 flex flex-col">
             <DepartureCard
+              title="出発地点"
+              time="到着時間目安"
+              location="場所"
+            />
+            <DestinationCard
               title="出発地点"
               time="到着時間目安"
               location="場所"
---
+++ frontend/src/components/Molecules/DestinationCard/index.tsx
@@ -0,0 +1,65 @@
+"use client";
+import React from "react";
+import { PlaceInput } from "@/components/ui/placeinput";
+import { DatePickerDemo } from "@/components/ui/DatePicker";
+import { Clock4 } from "lucide-react";
+
+interface DestinationCardProps {
+  title: string;
+  time: string;
+  location: string;
+}
+
+const DestinationCard: React.FC<DestinationCardProps> = ({
+  title,
+  time,
+  location,
+}) => {
+  const handleClick = (event: React.MouseEvent<HTMLInputElement>) => {
+    event.currentTarget.showPicker();
+  };
+
+  return (
+    <ul className="destination-card">
+      <div className="mb-[16px]">
+        <h2 className="text-[var(--text-border-default,#3F3F46)] font-noto text-[20px] font-normal leading-normal">
+          最終地点
+        </h2>
+      </div>
+      <div className="mb-[16px]">
+        <p className="text-[var(--text-border-default,#3F3F46)] font-noto text-[16px] font-normal leading-normal mb-[12px]">
+          場所
+        </p>
+        <PlaceInput />
+      </div>
+      <div>
+        <p className="text-[var(--text-border-default,#3F3F46)] font-noto text-[16px] font-normal leading-normal mb-[12px]">
+          出発時間目安
+        </p>
+        <div className="flex space-x-[16px]">
+          <DatePickerDemo />
+          <div className="relative flex items-center w-[102px] h-[43px]">
+            <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+              <Clock4 className="h-5 w-5 text-gray-400" />
+            </span>
+            <input
+              type="time"
+              onClick={handleClick}
+              className="pl-10 h-10 w-full rounded-md border border-input bg-background text-sm ring-offset-background focus-visible:outline-none focus-visible:ring-ring focus-visible:ring-offset-2"
+            />
+            <style jsx>{`
+              input[type="time"]::-webkit-calendar-picker-indicator {
+                display: none;
+              }
+              input[type="time"]:focus {
+                outline: none;
+              }
+            `}</style>
+          </div>
+        </div>
+      </div>
+    </ul>
+  );
+};
+
+export default DestinationCard;
