d07c17f06d867d114543d8ab76e58a7e8552378d
taku072002T <taku07200@gmail.com>
2025-02-25 23:34:17 +0900

    Edit serializers to add ScheduleAdjustSerializer.

--- backend/myapi/serializers.py
+++ backend/myapi/serializers.py
@@ -10,3 +10,36 @@ class TwoPlaceDistanceSerializer(serializers.Serializer):
     place_from = serializers.CharField()
     place_to = serializers.CharField()
     mode = serializers.CharField()
+
+
+class StartPointSerializer(serializers.Serializer):
+    location = serializers.CharField(max_length=100)
+    departureDateTime = serializers.DateTimeField()
+    travelMethodToNext = serializers.CharField(max_length=50)
+
+
+class ViaPointSerializer(serializers.Serializer):
+    index = serializers.IntegerField()
+    location = serializers.CharField(max_length=100)
+    arrivalDateTime = serializers.DateTimeField(required=False)
+    departureDateTime = serializers.DateTimeField(required=False)
+    priority = serializers.CharField(max_length=10, required=False)
+    travelMethodToNext = serializers.CharField(max_length=50, required=False)
+
+
+class FinalPointSerializer(serializers.Serializer):
+    location = serializers.CharField(max_length=100)
+    arrivalDateTime = serializers.DateTimeField()
+
+
+class ScheduleSerializer(serializers.Serializer):
+    startPoint = StartPointSerializer()
+    viaPoints = ViaPointSerializer(many=True)
+    finalPoint = FinalPointSerializer()
+
+
+class ScheduleAdjustSerializer(serializers.Serializer):
+    schedule = ScheduleSerializer()
+    now_time = serializers.DateTimeField()
+    passed_index = serializers.IntegerField()
+    delete = serializers.BooleanField()
