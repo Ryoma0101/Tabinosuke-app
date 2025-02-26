faf2ee62b6971430a320ec510e84664a5dc3f920
taku072002T <taku07200@gmail.com>
2025-02-26 22:10:39 +0900

    adde

--- backend/myapi/urls.py
+++ backend/myapi/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import PlaceNameView
 from .views import TwoPlaceDistanceView
+from .views import ScheduleAdjustmentView
 
 urlpatterns = [
     path('search-placename/', PlaceNameView.as_view(), name='placename'),
-    path('two-place-distance/', TwoPlaceDistanceView.as_view(), name='distance')
+    path('two-place-distance/', TwoPlaceDistanceView.as_view(), name='distance'),
+    path('schedule-adjustment/', ScheduleAdjustmentView.as_view(), name='schedule')
 ]
