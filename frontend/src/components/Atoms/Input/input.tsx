c8b7a0a695d13c18b005ba1ff7dc639ba0a0110e
Ryo <jukenohtani@gmail.com>
2025-02-28 00:57:30 +0900

    えらー解消

--- frontend/src/components/Atoms/Input/input.tsx
+++ frontend/src/components/Atoms/Input/input.tsx
@@ -5,21 +5,23 @@ import { cn } from "@/lib/utils";
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
 
-const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
-    return (
-      <input
-        type={type}
-        className={cn(
-          "flex h-[42px] w-[326px] rounded-sm border input border-[#3F3F46] bg-background px-4 py-[11px] text-[16px] text-[#A1A1AA],font-sans]",
-          className
-        )}
-        ref={ref}
-        {...props}
-      />
-    );
-  }
-);
+const Input = React.forwardRef<
+  HTMLInputElement,
+  React.InputHTMLAttributes<HTMLInputElement>
+>(({ className, type, ...props }, ref) => {
+  return (
+    <input
+      type={type}
+      className={cn(
+        "flex h-[42px] w-[326px] rounded-sm border input border-[#3F3F46] bg-background px-4 py-[11px] text-[16px] text-[#A1A1AA],font-sans]",
+        className,
+      )}
+      ref={ref}
+      {...props}
+    />
+  );
+});
+
 Input.displayName = "Input";
 
 export { Input };
