8777967f2219f5e4e8eb96650c7c2c525a6e53b4
taku072002T <taku07200@gmail.com>
2025-03-01 11:51:03 +0900

    Added management commnad

--- backend/myapi/management/commands/init_data.py
+++ backend/myapi/management/commands/init_data.py
@@ -11,14 +11,14 @@ class Command(BaseCommand):
         start_point = StartPoint.objects.create(
             id=uuid.UUID('12345678-1234-5678-1234-567812345678'),
             location="ユニークいくぜ",
-            departure_datetime=datetime.fromisoformat("2025-02-28T18:00:00"),
+            departure_datetime=datetime.fromisoformat("2025-03-01T12:00:00"),
             travel_method_to_next="車"
         )
 
         final_point = FinalPoint.objects.create(
             id=uuid.UUID('87654321-4321-8765-4321-876543218765'),
             location="ユニークついたぜ",
-            arrival_datetime=datetime.fromisoformat("2025-02-29T03:00:00")
+            arrival_datetime=datetime.fromisoformat("2025-03-02T18:00:00")
         )
 
         travel_plan = TravelPlan.objects.create(
@@ -29,10 +29,10 @@ class Command(BaseCommand):
         )
 
         via_points_data = [
-            {"id": uuid.UUID('11111111-1111-1111-1111-111111111111'), "index": 1, "location": "ユニーク1", "arrival_datetime": "2025-02-28T19:00:00", "priority": "高", "departure_datetime": "2025-02-28T20:00:00", "travel_method_to_next": "徒歩"},
-            {"id": uuid.UUID('22222222-2222-2222-2222-222222222222'), "index": 2, "location": "ユニーク2", "arrival_datetime": "2025-02-28T21:00:00", "priority": "中", "departure_datetime": "2025-02-28T22:00:00", "travel_method_to_next": "公共交通機関"},
-            {"id": uuid.UUID('33333333-3333-3333-3333-333333333333'), "index": 3, "location": "ユニーク3", "arrival_datetime": "2025-02-28T23:00:00", "priority": "中", "departure_datetime": "2025-02-29T00:00:00", "travel_method_to_next": "公共交通機関"},
-            {"id": uuid.UUID('44444444-4444-4444-4444-444444444444'), "index": 4, "location": "ユニーク4", "arrival_datetime": "2025-02-29T01:00:00", "priority": "低", "departure_datetime": "2025-02-29T02:00:00", "travel_method_to_next": "自転車"}
+            {"id": uuid.UUID('11111111-1111-1111-1111-111111111111'), "index": 1, "location": "ユニーク1", "arrival_datetime": "2025-03-01T19:00:00", "priority": "高", "departure_datetime": "2025-03-01T20:00:00", "travel_method_to_next": "徒歩"},
+            {"id": uuid.UUID('22222222-2222-2222-2222-222222222222'), "index": 2, "location": "ユニーク2", "arrival_datetime": "2025-03-01T21:00:00", "priority": "中", "departure_datetime": "2025-03-01T22:00:00", "travel_method_to_next": "公共交通機関"},
+            {"id": uuid.UUID('33333333-3333-3333-3333-333333333333'), "index": 3, "location": "ユニーク3", "arrival_datetime": "2025-03-01T23:00:00", "priority": "中", "departure_datetime": "2025-03-02T00:00:00", "travel_method_to_next": "公共交通機関"},
+            {"id": uuid.UUID('44444444-4444-4444-4444-444444444444'), "index": 4, "location": "ユニーク4", "arrival_datetime": "2025-03-02T15:00:00", "priority": "低", "departure_datetime": "2025-03-02T17:00:00", "travel_method_to_next": "自転車"}
         ]
 
         for via_point_data in via_points_data:
